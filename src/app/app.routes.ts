e38fa93302e31c1763d53e44dde3bcd24205551c
unsantiago <lprietoa2@gmail.com>
2024-05-28 21:04:20 -0500

    Commit de backup Angular18 actualizado nueva versión - Santiago

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,3 @@
 import { Routes } from '@angular/router';
-import { loggedGuard } from './core/guards/logged.guard';
 
-export const routes: Routes = [
-    {
-        path: '',
-        loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES),
-        canActivate: [loggedGuard]
-    },
-    {
-        path: 'articles',
-        loadChildren: () => import('./articles/articles.routes').then(m => m.ARTICLES_ROUTES)
-    },
-    {
-        path: 'dashboard',
-        loadChildren: () => import('./admin/admin.routes').then(m => m.ADMIN_ROUTES)
-    }
-];
+export const routes: Routes = [];
--- src/app/core/models/article.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface Article {
-    id: string;
-    title: string;
-    description: string;
-    body: string;
-    author: string;
-    published: boolean;
-    createdAt: Date;
-}
