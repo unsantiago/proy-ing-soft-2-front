45857faac15c5ad989bb8758f20e2dd4957318e3
unsantiago <lprietoa2@gmail.com>
2024-05-29 15:21:03 -0500

    Navbar y home terminado con test - Santiago

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -5,18 +5,7 @@ import {HousingLocation} from '../housinglocation';
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <section class="listing">
-      <img
-        class="listing-photo"
-        [src]="housingLocation.photo"
-        alt="Exterior photo of {{ housingLocation.name }}"
-        crossorigin
-      />
-      <h4 class="listing-heading">{{ housingLocation.name }}</h4>
-      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
-    </section>
-  `,
+  templateUrl: './housing-location.component.html',
   styleUrls: ['./housing-location.component.css'],
 })
 export class HousingLocationComponent {
